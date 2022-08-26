f3f3b57d209175dedcee0ab028bd0af7850b9cca
danielpclin <daniel.pclin@gmail.com>
2022-08-26 22:59:41 +0800

    Add functionality to export SVG

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Install all dependencies
+`yarn` or `yarn install`
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { toSvg } from 'html-to-image';
+const download = require("downloadjs");
+
 
 function App() {
+  const ref: React.MutableRefObject<HTMLElement|any> = useRef(null);
+
+  const exportAsSvg = () => {
+    toSvg(ref.current)
+        .then(function (dataUrl) {
+          download(dataUrl, 'my-node.png');
+        });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" ref={ref}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. <br />
+          Press the button below to export page as svg.
         </p>
         <a
           className="App-link"
@@ -18,6 +31,9 @@ function App() {
         >
           Learn React
         </a>
+        <button className="ExportSvgBtn" onClick={exportAsSvg}>
+          Export SVG
+        </button>
       </header>
     </div>
   );
